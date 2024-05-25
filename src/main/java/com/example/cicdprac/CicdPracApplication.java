c36237030be0b77ec0e15736a9c96873331c8d8c
minwoo0419 <ghdl464@naver.com>
2024-05-25 15:07:38 +0900

    first

---
+++ src/main/java/com/example/cicdprac/CicdPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdPracApplication.class, args);
+    }
+
+}
